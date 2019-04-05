c73e9a436ed9a8204197e1cc52adaea7372bc165
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-05 01:33:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,12 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = { "Dog" => bark}
+  your_hash = {
+    "Dog" => "bark",
+    "Cat" => "purr",
+    "Bird" => "chirp"
+  }
 
-  puts my_hash
+  puts my_
 
 end
 
