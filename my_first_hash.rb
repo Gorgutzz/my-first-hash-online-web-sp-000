f5cdc4e104bdeb86c4ba9e35850aa1f6278ab7be
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-05 01:43:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,6 +39,10 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["muskets"] = 4
+                 
   # add 2 muskets to the shipping_manifest hash below
 
 
